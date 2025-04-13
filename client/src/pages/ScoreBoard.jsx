f6f4875f716332d48c7455a62e90537856136ecd
VictordelaM <delamotte.v@gmail.com>
2025-04-13 23:14:57 +0200

    scoreboard changed to table and getSessionData fix

--- client/src/pages/ScoreBoard.jsx
+++ client/src/pages/ScoreBoard.jsx
@@ -10,27 +10,50 @@ const ScoreBoard = () => {
     useEffect(() =>{
         
         const getData = async() =>{
+            
             const sessionData = await getSessionData(quizId, sessionId)
+            console.log(sessionData)
             setSession(sessionData)
         }
         getData()
-    })
+    },[])
     const navigateCat = () =>{
         navigate('/selectCategory/quiz/'+quizId+'/session/'+sessionId)
     }
     return (
-        <div>
+        <div className='flex flex-col'>
             <Nav/>
-            {session?.participants?.map((participant)=>{
-                let points = 0
-                participant.points.map((p)=>{points += p.points})
-                return <div key={participant._id} className="scoreBox">
-                    <img className='avatar' url={participant.userImg} ></img>
-                    <p>{participant.username}</p>
-                    <p>{points}</p>
-                </div>
-            })}
-            <button onClick={navigateCat}>next category</button>
+            <div className='flex justify-evenly h-screen '>
+                <table className="border-collapse border border-gray-400 ...">
+                        <thead>
+                            <tr>
+                            <th className="border border-gray-300 ...">Teilnehmer</th>
+                            <th className="border border-gray-300 ...">Punkte</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                    {session?.participants?.map((participant)=>{
+                        let points = 0
+                        
+                        participant.points.map((p)=>{points += p.points})
+                        return <tr key={participant._id}>
+                            <td className="border border-gray-300 ..."><img className='avatar' url={participant.userImg} ></img>{participant.username}</td>
+                            <td className="border border-gray-300 ...">Detroit</td>
+                            </tr>
+                        
+                        // <div key={participant._id} className="flex flex-col test w-30/100">
+                        //     <img className='avatar' url={participant.userImg} ></img>
+                        //     <p>{participant.username}</p>
+                        //     <p>{points}</p>
+                        // </div>
+                    })}
+                    </tbody>
+                </table>
+                <button onClick={navigateCat}>next category</button>
+            </div>
+            
+            
         </div>
     )
 }
--- server/quiz/quizController/getSessionData.js
+++ server/quiz/quizController/getSessionData.js
@@ -4,7 +4,8 @@ export const getSessionData = async (req, res) =>{
     try {
         const { quizId, sessionId } = req.params; 
         const quiz = await Quiz.findOne({ quizId: quizId }); 
-        const session = quiz.sessions.find((session) => sessionId == sessionId)
+        const session = quiz.sessions.find((session) => session.sessionId == sessionId)
+        
         res.json(session);
     } catch (error) {
         console.error(error);
