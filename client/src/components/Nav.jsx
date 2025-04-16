a66ae619917915eef08dc2a95886eeeb8d5e0e4f
VictordelaM <delamotte.v@gmail.com>
2025-04-16 14:54:22 +0200

    logo darkmode

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,7 +4,8 @@ import home from '../assets/home-svgrepo-com.svg'
 import leftArrow from '../assets/arrow-sm-left-svgrepo-com.svg'
 import './stylesheets/Nav.css'
 import { useNavigate } from 'react-router-dom'
-import logo from '../assets/F413514D-5D3F-4D94-8CDD-3B42D5A1A9881744663182-removebg-preview.png'
+import logoDark from '../assets/F413514D-5D3F-4D94-8CDD-3B42D5A1A9881744663182-removebg-preview.png'
+import logoLight from '../assets/B9895F82-0799-4D0C-B0AE-14E18E410421.jpg'
 
 const Nav = () => {
     const [user, setUser] = useState(null);
@@ -32,9 +33,20 @@ const Nav = () => {
         <div className="navBox sticky flex bg-[var(--secondary-colour)] h-[5vh] justify-between px-[5%] text-[var(--primary-colour)]">
             
             <div className="flex justify-center items-center gap-[5%]  ">
-                <img src={leftArrow} alt="home" className='h-6 w-6 ' onClick={navBack}/>
+                <img src={leftArrow} alt="back" className='h-6 w-6 ' onClick={navBack}/>
             </div>
-            <img src={logo} alt="" className='w-[5vh] h-[5vh]'  onClick={navHome}/>
+            <img
+                src={logoLight}
+                alt="Logo Light"
+                className="w-[5vh] h-[5vh] block dark:hidden"
+                onClick={navHome}
+            />
+            <img
+                src={logoDark}
+                alt="Logo Dark"
+                className="w-[5vh] h-[5vh] hidden dark:block"
+                onClick={navHome}
+            />
             <div className='userCard flex justify-center items-center gap-[5%]' onClick={navSettings}>
                 <p className='text-[1.25rem]'>{user?.username}</p>
                 <img className="relative inline-block h-6 w-6 !rounded-full  object-cover object-center " src={user?.pictureUrl} alt="no img"/>       
