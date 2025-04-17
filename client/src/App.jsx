2a286300bad7374920087031ddb99774a2d59b4f
VictordelaM <delamotte.v@gmail.com>
2025-04-17 03:23:18 +0200

    deleted test and devInput

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import QuestionPage from './pages/QuestionPage'
 import LoginPage from './pages/LoginPage'
-import DevInput from './pages/DevInput'
 import UserAnswer from './pages/UserAnswer'
 import Index from './pages/Index'
 import SelectQuiz from './pages/SelectQuiz'
 import SelectCategory from './pages/SelectCategory'
 import QuizPage from './pages/QuizPage'
 import ScoreBoard from './pages/ScoreBoard'
-import Test from './components/Test'
 import RegisterPage from './pages/RegisterPage'
 import UserSettingPage from './pages/UserSettingPage'
 import AGB from './pages/AGB'
@@ -33,10 +31,8 @@ function App() {
         <Route path="/login" element= {<LoginPage/>}/>
         <Route path="/register" element= {<RegisterPage/>}/>
         <Route path="/userSettings" element= {<UserSettingPage/>}/>
-        <Route path="/devInput" element= {<DevInput/>}/>
         <Route path="/selectQuiz" element= {<SelectQuiz/>}/>
-        <Route path="/scoreBoard/quiz/:quizId/session/:sessionId" element={<ScoreBoard />} />
-        <Route path="/test" element={<Test />} />        
+        <Route path="/scoreBoard/quiz/:quizId/session/:sessionId" element={<ScoreBoard />} />      
         <Route path="/agb" element={<AGB />} />
         <Route path="/dsvo" element={<Datenschutz />} />
         <Route path="/addQuiz" element={<AddQuiz/>}/>
